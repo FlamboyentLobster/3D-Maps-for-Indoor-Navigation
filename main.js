b960b7e5fff76358673f861f4631f1eba682388e
FlamboyentLobster <72074757+FlamboyentLobster@users.noreply.github.com>
2024-01-17 17:37:23 +0000

    Created multiple walls

--- main.js
+++ main.js
@@ -21,10 +21,38 @@ const light = new THREE.DirectionalLight( 0xffffff, 1 );
 light.position.set( 20, 30, 60 ); //default; light shining from top
 light.castShadow = true; // default false
 
-const geometry = new THREE.BoxGeometry(0, 5, 10, 10);
-const material = new THREE.MeshBasicMaterial({ color: 0xADD8E6, transparent: true});
-const wall = new THREE.Mesh(geometry, material);
-wall.receiveShadow = true;
+const gridHelper = new THREE.GridHelper(70, 70);
+
+var floors = 3;
+var wallInstances = 8;
+
+let wallPos = [
+    [10, 2.5, 0, 0], 
+    [0, 2.5, 10, Math.PI / 2],
+    [-10, 2.5, 0, 0],
+    [0, 2.5, -10, Math.PI / 2],
+    [10, 10, 0, 0], 
+    [0, 10, 10, Math.PI / 2],
+    [-10, 10, 0, 0],
+    [0, 10, -10, Math.PI / 2]
+    ];
+
+const tempWall = new THREE.Object3D();
+
+const geometry = new THREE.BoxGeometry(0, 5, 20, 10);
+const material = new THREE.MeshBasicMaterial({ color: 0xADD8E6 });
+const wall = new THREE.InstancedMesh(geometry, material, wallInstances);
+scene.add(wall, gridHelper);
+
+for (let i = 0; i < wallInstances; i++) {
+  tempWall.position.x = wallPos[i][0];
+  tempWall.position.y = wallPos[i][1];
+  tempWall.position.z = wallPos[i][2];
+  tempWall.rotation.y = wallPos[i][3];
+
+  tempWall.updateMatrix();
+  wall.setMatrixAt(i, tempWall.matrix)
+}
 
 const edges = new THREE.EdgesGeometry(geometry); 
 const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x000071 } ) ); 
@@ -33,11 +61,6 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 15;
 controls.maxDistance = 70;
 
-const gridHelper = new THREE.GridHelper(70, 70);
-
-scene.add(wall, gridHelper);
-wall.position.set(0, 2.5, 0);
-
 function animate() {
  requestAnimationFrame(animate);
  controls.update();
@@ -45,5 +68,3 @@ function animate() {
 }
 
 animate()
-
-
