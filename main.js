4c9de63f99e491c383d43e4bb54c2c61e4e5bddb
FlamboyentLobster <72074757+FlamboyentLobster@users.noreply.github.com>
2024-01-18 17:59:03 +0000

    Changed size and scale of walls

--- main.js
+++ main.js
@@ -27,21 +27,27 @@ var floors = 3;
 var wallInstances = 8;
 
 let wallPos = [
-    [10, 2.5, 0, 0], 
-    [0, 2.5, 10, Math.PI / 2],
-    [-10, 2.5, 0, 0],
-    [0, 2.5, -10, Math.PI / 2],
-    [10, 10, 0, 0], 
-    [0, 10, 10, Math.PI / 2],
-    [-10, 10, 0, 0],
-    [0, 10, -10, Math.PI / 2]
+    [10, 2.5, 0, 0, 1, 1, 2], 
+    [0, 2.5, 10, Math.PI / 2, 1, 1, 1],
+    [-10, 2.5, 0, 0, 1, 1, 1],
+    [0, 2.5, -10, Math.PI / 2, 1, 1, 1],
+    [10, 10, 0, 0, 1, 1, 1], 
+    [0, 10, 10, Math.PI / 2, 1, 1, 1],
+    [-10, 10, 0, 0, 1, 1, 1],
+    [0, 10, -10, Math.PI / 2, 1, 1, 1]
     ];
 
 const tempWall = new THREE.Object3D();
 
-const geometry = new THREE.BoxGeometry(0, 5, 20, 10);
+const geometry = new THREE.BoxGeometry(0, 5, 5, 10);
 const material = new THREE.MeshBasicMaterial({ color: 0xADD8E6 });
 const wall = new THREE.InstancedMesh(geometry, material, wallInstances);
+
+const geo = new THREE.WireframeGeometry( geometry ); // or WireframeGeometry
+const mat = new THREE.LineBasicMaterial( { color: 0x000000 } );
+const wireframe = new THREE.LineSegments( geo, mat, wallInstances );
+wall.add(wireframe);
+
 scene.add(wall, gridHelper);
 
 for (let i = 0; i < wallInstances; i++) {
@@ -49,13 +55,14 @@ for (let i = 0; i < wallInstances; i++) {
   tempWall.position.y = wallPos[i][1];
   tempWall.position.z = wallPos[i][2];
   tempWall.rotation.y = wallPos[i][3];
+  tempWall.scale.x = wallPos[i][4];
+  tempWall.scale.y = wallPos[i][5];
+  tempWall.scale.z = wallPos[i][6];
 
   tempWall.updateMatrix();
   wall.setMatrixAt(i, tempWall.matrix)
+  //wireframe.setMatrixAt(i, tempWall.matrix)
 }
-
-const edges = new THREE.EdgesGeometry(geometry); 
-const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial( { color: 0x000071 } ) ); 
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 15;
