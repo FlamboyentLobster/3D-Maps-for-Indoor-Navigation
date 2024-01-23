cc54270710bd3b9dc13e28a87e28c844f8738d0b
FlamboyentLobster <72074757+FlamboyentLobster@users.noreply.github.com>
2024-01-23 13:30:42 +0000

    Added buttons

--- src/main.js
+++ src/main.js
@@ -9,26 +9,26 @@ import walls from './Walls';
 import floors from './Floors';
 //import stairs from './Stairs';
 
-const CSSRenderer = new CSS2DRenderer();
-CSSRenderer.setSize(window.innerWidth,window.innerHeight);
-CSSRenderer.domElement.style.position = 'absolute';
-CSSRenderer.domElement.style.top = '0px';
-CSSRenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(CSSRenderer.domElement);
+//onst CSSRenderer = new CSS2DRenderer();
+//CSSRenderer.setSize(window.innerWidth,window.innerHeight);
+//CSSRenderer.domElement.style.position = 'absolute';
+//CSSRenderer.domElement.style.top = '0px';
+//CSSRenderer.domElement.style.pointerEvents = 'none';
+//document.body.appendChild(CSSRenderer.domElement);
 
-const upButton = document.createElement('button');
-upButton.type = 'button';
-upButton.addEventListener('click', function(){
-    
+//const upButton = document.createElement('button');
+//upButton.type = 'button';
+//upButton.addEventListener('click', function(){
+   
     // scene.remove(measurementLabels[close_button.id]);
-});
+//});
 //const label = new CSS2DObject(point);
 //scene.add(label);
 
-const div = document.createElement('div');
+//const div = document.createElement('div');
 //div.appendChild(upButton);
-const divContainer = new CSS2DObject(div);
-scene.add(divContainer);
+//const divContainer = new CSS2DObject(div);
+//scene.add(divContainer);
 
 const light = new THREE.DirectionalLight( 0xffffff, 1 );
 light.position.set( 20, 30, 60 ); //default; light shining from top
@@ -45,7 +45,7 @@ controls.maxDistance = 60;
 function animate() {
  requestAnimationFrame(animate);
  controls.update();
- CSSRenderer.render(scene, camera);
+ //CSSRenderer.render(scene, camera);
  renderer.render(scene, camera);
 }
 
