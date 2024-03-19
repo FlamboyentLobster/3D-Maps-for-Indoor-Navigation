64e815632ff34e367249c10c84ef2393b6a075f2
FlamboyentLobster <72074757+FlamboyentLobster@users.noreply.github.com>
2024-03-19 11:42:55 +0000

    deleted not needed variables

--- src/Camera.js
+++ src/Camera.js
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 
-var currentFloor = 0;
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.setZ(20);
 camera.position.setY(10);
